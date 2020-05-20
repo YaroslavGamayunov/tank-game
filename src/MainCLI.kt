dfb18cff9d9bae69fd23d1a3aded347f39fe29dd
Gregory Minakov <gaoadt@outlook.com>
2020-05-20 18:29:44 +0300

    GUI Clients are conected

--- src/MainCLI.kt
+++ src/MainCLI.kt
@@ -10,7 +10,7 @@ import guiclient.swing.SwingRenderingContext
 import guiclient.swing.SwingSceneRenderer
 
 fun main(){
-    runGuiSinglePlayer()
+    runGUIMult()
 }
 
 fun runGuiSinglePlayer(){
@@ -20,6 +20,30 @@ fun runGuiSinglePlayer(){
     scene.renderer = SwingSceneRenderer(scene)
     val client = GUIClient<SwingRenderingContext>(connector, scene, swingContext)
     connector.runConnector(client)
+}
+
+fun runGUIMult(){
+    val server = LocalMultiplayerServer()
+
+    val connector1 = LocalMultiplayerConnector(server, tankPosition = Vector2(0,0))
+    val swingContext1 = SwingRenderingContext()
+    val scene1 = RenderingScene<SwingRenderingContext>()
+    scene1.renderer = SwingSceneRenderer(scene1)
+    val client1 = GUIClient<SwingRenderingContext>(connector1, scene1, swingContext1)
+    connector1.runConnector(client1)
+
+    val connector2 = LocalMultiplayerConnector(server, tankPosition = Vector2(3,0))
+    val swingContext2 = SwingRenderingContext()
+    val scene2 = RenderingScene<SwingRenderingContext>()
+    scene2.renderer = SwingSceneRenderer(scene2)
+    val client2 = GUIClient<SwingRenderingContext>(connector2, scene2, swingContext2)
+    connector2.runConnector(client2)
+/*
+    val connector2 = LocalMultiplayerConnector(server, tankPosition = Vector2(3,0))
+    val client2 = CLIGameClient(connector2)
+    connector2.runConnector(client2)
+*/
+    server.startGame()
 }
 
 fun runMultiplayer(){
